34653a7ffa4a9707297a5fdf0b87dd16ebf1062d
colinjfarmer <81653945+colinjfarmer@users.noreply.github.com>
2022-10-21 15:01:15 -0700

    Update backgammon_dsbg.py

--- agents/backgammon_dsbg.py
+++ agents/backgammon_dsbg.py
@@ -1,77 +1,112 @@
 '''
-Name(s): Jordan White, Colin Farme
-UW netid(s): JWHITE34, CFARME
+Name(s): Colin Farmer, Jordan White
+UW netid(s): cfarme, jwhite34
 '''
 
-from game_engine import genmoves
+from game_engine import genmoves 
 
 class BackgammonPlayer:
     def __init__(self):
         self.GenMoveInstance = genmoves.GenMoves()
+        self.max_depth = 2
+        self.states_explored = 0
+        self.cuttoffs = 0
         # feel free to create more instance variables as needed.
+    
+    def initialize_move_gen_for_state(self, state, who, die1, die2):
+        self.move_generator = self.GenMoveInstance.gen_moves(state, who, die1, die2)
 
+    
     # TODO: return a string containing your UW NETID(s)
     # For students in partnership: UWNETID + " " + UWNETID
     def nickname(self):
-        return "JWHITE34"+" "+"CFARME"
+        # TODO: return a string representation of your UW netid(s)
+        return "cfarme + jwhite34"
 
     # If prune==True, then your Move method should use Alpha-Beta Pruning
     # otherwise Minimax
     def useAlphaBetaPruning(self, prune=False):
         # TODO: use the prune flag to indiciate what search alg to use
-        pass
+        if prune:
+            # Run Alpha Beta
+            self.move()
+        else:
+            # Run Minimax
+            self.move()
+        
 
     # Returns a tuple containing the number explored
     # states as well as the number of cutoffs.
     def statesAndCutoffsCounts(self):
         # TODO: return a tuple containig states and cutoff
-        return (-1, -1)
+        return(self.states_explored,self.cuttoffs)
 
     # Given a ply, it sets a maximum for how far an agent
     # should go down in the search tree. maxply=2 indicates that
     # our search level will go two level deep.
     def setMaxPly(self, maxply=2):
-        # TODO: set the max ply
-        pass
+        self.max_depth = maxply
 
-    # If not None, it updates the internal static evaluation
+    # If not None, it update the internal static evaluation
     # function to be func
     def useSpecialStaticEval(self, func):
-        if self is not None:
-            staticEval(self, func)
-        pass
-
-    # Given a state and a roll of dice, it returns the best move for
-    # the state.whose_move.
-    # Keep in mind: a player can only pass if the player cannot move any checker with that role
-    def move(self, state, die1=1, die2=6):
-        # TODO: return a move for the current state and for the current player.
-        # Hint: you can get the current player with state.whose_move
-        best = -99999
-        if self.whose_move == 'W':
-
-        return "q"
-
+        # TODO: update your staticEval function appropriately
+        if func != None:
+            def staticEval(self,state): func
+        
+    
     # checks legality of a move
     def legalMove(self, state, index, die):
         for i in range(die):
             if (self.pointLists[index+i+1][0] == 1 - self.pointLists[index][0]) and (self.pointLists[index+i+1][0].length >= 2):
                 return False
         return True
+    
+    # Given a state and a roll of dice, it returns the best move for
+    # the state.whose_move.
+    # Keep in mind: a player can only pass if the player cannot move any checker with that role
+    def move(self, state, die1=1, die2=6):
+        # TODO: return a move for the current state and for the current player.
+        # Hint: you can get the current player with state.whose_move
+        highest_score = -100000
+        # Look at all possible moves
+            # If that move is legal
+                # Get the score for that move
+                # If this move has the best relative score, then take that move and save if
+        
+        # return the best move for a given state
+        return "q"
 
 
     # Hint: Look at game_engine/boardState.py for a board state properties you can use.
-    # Calculates pip counts for red and white and returns the difference, white being positive
     def staticEval(self, state):
-        whitePip = 0;
-        redPip = 0;
-        itr = 1;
-        for i in self.pointLists:
+        # TODO: return a number for the given state
+        white_pip = 0
+        red_pip = 0
+        itr = 1
+        for i in self.pointList:
             if(i[0] == 0):
-                whitePip = whitePip + (i.length)*(25-itr)
-            elif (i[0] == 1):
-                redPip = redPip+(i.length)*(itr)
-            itr = itr + 1
-
-        return whitePip - redPip
-
+                white_pip = white_pip + len(i)*(25-itr)
+            elif(i[0] == 1):
+                red_pip = red_pip + len(i) * itr
+        # any blocked pieaces
+        if state.whose_move == 0: return red_pip - white_pip
+        else: return white_pip-red_pip
+        
+    def get_all_possible_moves(self):
+        """Uses the mover to generate all legal moves. Returns an array of move commands"""
+        move_list = []
+        done_finding_moves = False
+        any_non_pass_moves = False
+        while not done_finding_moves:
+            try:
+                m = next(self.move_generator)    # Gets a (move, state) pair.
+                # print("next returns: ",m[0]) # Prints out the move.    For debugging.
+                if m[0] != 'p':
+                    any_non_pass_moves = True
+                    move_list.append(m[0])    # Add the move to the list.
+            except StopIteration as e:
+                done_finding_moves = True
+        if not any_non_pass_moves:
+            move_list.append('p')
+        return move_list
